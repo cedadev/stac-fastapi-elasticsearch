7f619b5cb4473a9174196aa4b6ff29d60fe1a484
rsmith013 <richard.d.smith@stfc.ac.uk>
2021-09-22 15:38:48 +0100

    adding skeleton for transactions client

--- stac_fastapi/elasticsearch/transactions.py
+++ stac_fastapi/elasticsearch/transactions.py
@@ -7,3 +7,106 @@ __date__ = '11 Jun 2021'
 __copyright__ = 'Copyright 2018 United Kingdom Research and Innovation'
 __license__ = 'BSD - see LICENSE file in top-level package directory'
 __contact__ = 'richard.d.smith@stfc.ac.uk'
+
+
+from stac_fastapi.types.core import BaseTransactionsClient
+
+
+class TransactionsClient(BaseTransactionsClient):
+    """Defines a pattern for implementing the STAC transaction extension."""
+
+    @abc.abstractmethod
+    def create_item(self, item: stac_types.Item, **kwargs) -> stac_types.Item:
+        """Create a new item.
+
+        Called with `POST /collections/{collectionId}/items`.
+
+        Args:
+            item: the item
+
+        Returns:
+            The item that was created.
+
+        """
+        ...
+
+    @abc.abstractmethod
+    def update_item(self, item: stac_types.Item, **kwargs) -> stac_types.Item:
+        """Perform a complete update on an existing item.
+
+        Called with `PUT /collections/{collectionId}/items`. It is expected that this item already exists.  The update
+        should do a diff against the saved item and perform any necessary updates.  Partial updates are not supported
+        by the transactions extension.
+
+        Args:
+            item: the item (must be complete)
+
+        Returns:
+            The updated item.
+        """
+        ...
+
+    @abc.abstractmethod
+    def delete_item(
+        self, item_id: str, collection_id: str, **kwargs
+    ) -> stac_types.Item:
+        """Delete an item from a collection.
+
+        Called with `DELETE /collections/{collectionId}/items/{itemId}`
+
+        Args:
+            item_id: id of the item.
+            collection_id: id of the collection.
+
+        Returns:
+            The deleted item.
+        """
+        ...
+
+    @abc.abstractmethod
+    def create_collection(
+        self, collection: stac_types.Collection, **kwargs
+    ) -> stac_types.Collection:
+        """Create a new collection.
+
+        Called with `POST /collections`.
+
+        Args:
+            collection: the collection
+
+        Returns:
+            The collection that was created.
+        """
+        ...
+
+    @abc.abstractmethod
+    def update_collection(
+        self, collection: stac_types.Collection, **kwargs
+    ) -> stac_types.Collection:
+        """Perform a complete update on an existing collection.
+
+        Called with `PUT /collections`. It is expected that this item already exists.  The update should do a diff
+        against the saved collection and perform any necessary updates.  Partial updates are not supported by the
+        transactions extension.
+
+        Args:
+            collection: the collection (must be complete)
+
+        Returns:
+            The updated collection.
+        """
+        ...
+
+    @abc.abstractmethod
+    def delete_collection(self, collection_id: str, **kwargs) -> stac_types.Collection:
+        """Delete a collection.
+
+        Called with `DELETE /collections/{collectionId}`
+
+        Args:
+            collection_id: id of the collection.
+
+        Returns:
+            The deleted collection.
+        """
+        ...

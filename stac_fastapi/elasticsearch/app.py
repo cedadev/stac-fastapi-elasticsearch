2699f0962a4068a204e651f9b25b798363481c46
Mahir-Sparkess <mahirrahman2000@gmail.com>
2021-10-22 16:27:22 +0100

    change api description 2.0

--- stac_fastapi/elasticsearch/app.py
+++ stac_fastapi/elasticsearch/app.py
@@ -39,7 +39,9 @@ api = StacApi(
     extensions=extensions,
     client=CoreCrudClient(session=session, extensions=extensions),
     get_pagination_model=GETPagination,
-    post_pagination_model=POSTPagination
+    post_pagination_model=POSTPagination,
+    description="TEST STAC API; this is experimental",
+    title="TEST"
 )
 
 app = api.app

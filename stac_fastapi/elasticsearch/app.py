4885a059adaba8b93f87c0646ff5e1b8094c801b
Mahir-Sparkess <mahirrahman2000@gmail.com>
2022-01-06 09:52:03 +0000

    change api description 2.0

--- stac_fastapi/elasticsearch/app.py
+++ stac_fastapi/elasticsearch/app.py
@@ -11,35 +11,36 @@ __contact__ = 'richard.d.smith@stfc.ac.uk'
 from stac_fastapi.api.app import StacApi
 from stac_fastapi.extensions.core import (
     ContextExtension,
-    FieldsExtension,
-    SortExtension,
-    FilterExtension
+    FilterExtension,
+    PaginationExtension
 )
+
+
 from stac_fastapi.elasticsearch.session import Session
 from stac_fastapi.elasticsearch.core import CoreCrudClient
 from stac_fastapi.elasticsearch.filters import FiltersClient
 from stac_fastapi.elasticsearch.config import settings
-from stac_fastapi.api.models import GETPagination, POSTPagination
+
 
 from stac_fastapi_freetext.free_text import FreeTextExtension
-from stac_fastapi_context_collections.context_collections import  ContextCollectionExtension
+from stac_fastapi_context_collections.context_collections import ContextCollectionExtension
 
 extensions = [
-        ContextExtension(),
-        # FieldsExtension(),
-        # SortExtension(),
-        FilterExtension(client=FiltersClient()),
-        FreeTextExtension(),
-        ContextCollectionExtension(),
-    ]
+    ContextExtension(),
+    # FieldsExtension(),
+    # SortExtension(),
+    FilterExtension(client=FiltersClient()),
+    FreeTextExtension(),
+    ContextCollectionExtension(),
+    PaginationExtension()
+]
 
 session = Session.create_from_settings(settings)
 api = StacApi(
-    settings = settings,
+    settings=settings,
     extensions=extensions,
     client=CoreCrudClient(session=session, extensions=extensions),
-    get_pagination_model=GETPagination,
-    post_pagination_model=POSTPagination,
+    pagination_extension=PaginationExtension,
     description=settings.STAC_DESCRIPTION,
     title=settings.STAC_TITLE,
 )
